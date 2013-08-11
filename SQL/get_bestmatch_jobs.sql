afb29bd1fad3044f2adf04022d7aa42580d1aaf7
abhihub <aarora2@gmail.com>
2013-08-10 17:15:28 -0700

    Added courses

--- SQL/get_bestmatch_jobs.sql
+++ SQL/get_bestmatch_jobs.sql
@@ -5,7 +5,9 @@ CREATE PROCEDURE  `fillskils`.`get_bestmatch_jobs`
 IN usersid INT
 )
 BEGIN
-SELECT COUNT(aa.jobid) as numberofskills, aa.jobid as jobid, jj.jobtitle as jobtitle, CONCAT(jj.`city`, " ,", jj.`state`) as location
+SELECT COUNT(aa.jobid) as numberofskills, aa.jobid as jobid, jj.url as url,
+jj.jobtitle as jobtitle, CONCAT(jj.`city`, " ,", jj.`state`)
+as location
 FROM (
 SELECT DISTINCT a.`skillid`, a.`userid`, b.`jobid` FROM 
 (select * from usersskills where userid = usersid) as a 
@@ -14,6 +16,6 @@ INNER JOIN jobsskills as b using (`skillid`)
 join
 jobs as jj on aa.`jobid` = jj.`id`
 group by aa.`jobid`
-order by count(aa.`jobid`) DESC LIMIT 5;
+order by count(aa.`jobid`) DESC LIMIT 6;
 END $$
 DELIMITER ;
