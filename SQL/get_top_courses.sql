f993a7dddeea0fb4f8857268a55ee210329aa336
abhihub <aarora2@gmail.com>
2013-08-12 15:00:14 -0700

    Only show top 3 courses
    Instead of showing all courses for first skill, only show top 3

--- SQL/get_top_courses.sql
+++ SQL/get_top_courses.sql
@@ -10,15 +10,19 @@ SELECT co.`name` as coursename, csk.courseid, missingskills.numberofskills,
 missingskills.skillname, co.`description`, co.`url` 
 FROM coursesskills csk 
 INNER JOIN skills sk ON csk.`skillid` = sk.`id` 
-inner join courses co on co.`id` = csk.`courseid`
-inner join
+INNER JOIN courses co on co.`id` = csk.`courseid`
+INNER JOIN
 	(
-	select count(dd.`missingskill`) as numberofskills, sk.`name` as skillname, group_concat(jobid) from
+	SELECT count(dd.`missingskill`) AS numberofskills, sk.`name` as skillname, group_concat(jobid) from
 	(
 	SELECT b.`skillid` as missingskill, b.`jobid`
-	FROM jobsskills as b left outer JOIN (select * from usersskills where userid = usersid) as usert using (`skillid`) where usert.`skillid` is NULL ) as dd join skills as sk on 		dd.`missingskill` = sk.`id`
-	group by dd.`missingskill`
-	order by count(dd.missingskill) desc
+	FROM jobsskills as b LEFT OUTER JOIN 
+		(select * from usersskills where userid = usersid) as usert using (`skillid`) 
+		 where usert.`skillid` is NULL ) as dd 
+		 join skills as sk 
+		 on dd.`missingskill` = sk.`id`
+		 group by dd.`missingskill`
+		 order by count(dd.missingskill) desc
 	) as missingskills on sk.`name` = missingskills.skillname
 order by missingskills.numberofskills desc limit 6;
 END $$
